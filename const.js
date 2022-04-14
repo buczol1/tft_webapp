64fd2c3d1c8c544508c74f5acda2e257dbc68baf
buczol1 <buczynskiartur6@gmail.com>
2022-04-15 01:53:17 +0200

    deleted api-key

--- const.js
+++ const.js
@@ -4,7 +4,7 @@ const constants = {
 		summonerByName: "/lol/summoner/v4/summoners/by-name/{name}?api_key={api_key}",
 		tftLeague: "/tft/league/v1/entries/by-summoner/{summoner_id}?api_key={api_key}"
 	},
-	api: 'RGAPI-ab83542a-5a21-4216-af45-def1238bfa56'
+	api: ''
 }
 
 module.exports = constants;
