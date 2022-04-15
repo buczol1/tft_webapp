38778f3d16ba497320fed6039b4baa000b9405ad
buczol1 <buczynskiartur6@gmail.com>
2022-04-15 07:55:20 +0200

    0.0.2

--- const.js
+++ const.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const constants = {
 	urls: {
 		urlStart: "https://eun1.api.riotgames.com",
-		summonerByName: "/lol/summoner/v4/summoners/by-name/{name}?api_key={api_key}",
+		summonerByName: "/tft/summoner/v1/summoners/by-name/{name}?api_key={api_key}",
 		tftLeague: "/tft/league/v1/entries/by-summoner/{summoner_id}?api_key={api_key}"
 	},
-	api: ''
+	api: process.env.API_KEY
 }
 
 module.exports = constants;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const urls = require('./const').urls;
-const apiCode = require('./const').api;
+const apiKey = require('./const').api;
 const fetch = require('node-fetch');
 
 
@@ -12,7 +12,7 @@ app.use(express.json({limit: '1mb'}));
 
 app.get('/summoner/:summName', async (request, response) => {
 	const summNameValue = request.params.summName;
-	const api_url = urls.urlStart + urls.summonerByName.replace('{name}',summNameValue).replace('{api_key}',apiCode);
+	const api_url = urls.urlStart + urls.summonerByName.replace('{name}',summNameValue).replace('{api_key}',apiKey);
 	const fetch_repsonse = await fetch(api_url);
 	const json = await fetch_repsonse.json();
 	response.json(json);
@@ -20,7 +20,7 @@ app.get('/summoner/:summName', async (request, response) => {
 
 app.get('/tft-info/:summID', async (request, response) => {
 	const summID = request.params.summID;
-	const api_url = urls.urlStart + urls.tftLeague.replace('{summoner_id}',summID).replace('{api_key}',apiCode);
+	const api_url = urls.urlStart + urls.tftLeague.replace('{summoner_id}',summID).replace('{api_key}',apiKey);
 	const fetch_repsonse = await fetch(api_url);
 	const json = await fetch_repsonse.json();
 	response.json(json[0]);
